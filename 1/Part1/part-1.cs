f6bfbd7f167fad9c377b128b731858210d4297a2
Russ Penska <russpenska@gmail.com>
2023-12-06 08:46:04 +0000

    Day1

---
+++ 1/Part1/part-1.cs
@@ -0,0 +1,28 @@
+﻿var lines = System.IO.File.ReadAllLines("input.txt");
+var sum = 0;
+
+foreach (var line in lines)
+{
+    var firstDigit = -1; // means no char yet
+    var lastDigit = -1;
+    foreach (var character in line)
+    {
+        var code = (int)character;
+        if (code >= 48 && code <= 57)
+        {
+            var digit = code - 48;
+            if (firstDigit < 0)
+            {
+                firstDigit = digit;
+            }
+
+            lastDigit = digit;
+        }
+    }
+
+    var combinedDigits = (firstDigit * 10) + lastDigit;
+    sum += combinedDigits;
+}
+
+
+System.Console.WriteLine(sum);
---
+++ 1/Part2/Program.cs
@@ -0,0 +1,51 @@
+﻿var lines = System.IO.File.ReadAllLines("input.txt");
+
+var digitRepresentations = new System.Collections.Generic.Dictionary<string, int>() {
+    { "1", 1 },
+    { "2", 2 },
+    { "3", 3 },
+    { "4", 4 },
+    { "5", 5 },
+    { "6", 6 },
+    { "7", 7 },
+    { "8", 8 },
+    { "9", 9 },
+    { "one", 1 },
+    { "two", 2 },
+    { "three", 3 },
+    { "four", 4 },
+    { "five", 5 },
+    { "six", 6 },
+    { "seven", 7 },
+    { "eight", 8 },
+    { "nine", 9 },
+};
+
+var sum = 0;
+foreach(var line in lines)
+{
+    var firstDigit = -1;
+    var lastDigit = -1;
+    
+    for (int i = 0; i < line.Length; i++)
+    {
+        foreach (var key in digitRepresentations.Keys)
+        {
+            if (key.Length <= line.Length - i && line.Substring(i, key.Length) == key)
+            {
+                if (firstDigit == -1)
+                {
+                    firstDigit = digitRepresentations[key];
+                }
+
+                lastDigit = digitRepresentations[key];
+            }
+        }
+    }
+
+    sum += (firstDigit * 10) + lastDigit;
+
+    System.Console.WriteLine(line + "\t" + firstDigit + "\t" + lastDigit);
+}
+
+System.Console.WriteLine(sum);
